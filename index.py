da5888a6493e993b8f039c55f8ca87a4c902156f
Rakotoarinosy <fehizoroarinosy@gmail.com>
2024-03-13 01:16:22 +0300

    tri à bulle et tri par séléction

---
+++ index.py
@@ -0,0 +1,14 @@
+################# Tri à Bulles #################
+
+def tri_a_bulles(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+arr = [64,90,34,25,12,22,11]
+tri_a_bulles(arr)
+print("Tableau trié: ")
+
+print(arr)
